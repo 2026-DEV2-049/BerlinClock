e9c29130a89983df43a4c9b20df85cc5887c22cb
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-18 18:54:15 +0100

    Start the MinuteConverter with empty UT for now

---
+++ BerlinClock/Core/MinuteConverter.swift
@@ -0,0 +1,13 @@
+//
+//  MinuteConverter.swift
+//  BerlinClock
+//
+//  Created by 2026-DEV2-049 on 18/02/2026.
+//
+
+
+import Foundation
+
+enum MinuteConverter {
+    
+}
---
+++ BerlinClock/CoreTests/MinuteConverterTests.swift
@@ -0,0 +1,19 @@
+//
+//  MinuteConverterTests.swift
+//  BerlinClock
+//
+//  Created by 2026-DEV2-049 on 18/02/2026.
+//
+
+
+import Testing
+import Foundation
+@testable import Core
+
+struct MinuteConverterTests {
+
+    @Test func firstFunc() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
