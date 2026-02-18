a69730101b738ead6a604774c11e234ec1d7d03b
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-18 18:25:48 +0100

    Add new tests for remaining values and make them fail

--- BerlinClock/CoreTests/HourConverterTests.swift
+++ BerlinClock/CoreTests/HourConverterTests.swift
@@ -23,5 +23,23 @@ struct HourConverterTests {
         let result = HourConverter.convert(23)
         #expect(result == "RRRO")
     }
+    
+    @Test("Convert hours to string succeeds when hour is 02")
+    func twoHourForSingleHours() {
+        let result = HourConverter.convert(2)
+        #expect(result == "OOOO")
+    }
+    
+    @Test("Convert hours to string succeeds when hour is 08")
+    func eightHourForSingleHours() {
+        let result = HourConverter.convert(8)
+        #expect(result == "RRRR")
+    }
+    
+    @Test("Convert hours to string succeeds when hour is 14")
+    func forteenHourForSingleHours() {
+        let result = HourConverter.convert(14)
+        #expect(result == "RRRO")
+    }
 
 }
