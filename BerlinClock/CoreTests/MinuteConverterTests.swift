fae0ef28dc16c0d5d5a1276375dde8ba811b2718
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-18 19:07:44 +0100

    Add expected values to UT --> Some are still failing

--- BerlinClock/CoreTests/MinuteConverterTests.swift
+++ BerlinClock/CoreTests/MinuteConverterTests.swift
@@ -23,25 +23,25 @@ struct MinuteConverterTests {
         @Test("Convert hours to string succeeds when minute is 59")
         func fiftyNineMinute() {
             let result = MinuteConverter.singleMinute(from: 59)
-            #expect(result == "OOOO")
+            #expect(result == "YYYY")
         }
         
         @Test("Convert hours to string succeeds when minute is 32")
         func thirtyTwoMinute() {
             let result = MinuteConverter.singleMinute(from: 32)
-            #expect(result == "OOOO")
+            #expect(result == "YYOO")
         }
         
         @Test("Convert hours to string succeeds when minute is 34")
         func thirtyFourMinute() {
             let result = MinuteConverter.singleMinute(from: 34)
-            #expect(result == "OOOO")
+            #expect(result == "YYYY")
         }
         
         @Test("Convert hours to string succeeds when minute is 35")
         func thirtyFiveMinute() {
             let result = MinuteConverter.singleMinute(from: 35)
-            #expect(result == "OROO")
+            #expect(result == "OOOO")
         }
     }
 }
