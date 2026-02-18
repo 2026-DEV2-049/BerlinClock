00b756427d6f837c4168a330a34eb1d245c28c5c
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-18 20:06:09 +0100

    Introduce a DateFactory in order to start the full conversion

--- BerlinClock/Core/BerlinClockRules.swift
+++ BerlinClock/Core/BerlinClockRules.swift
@@ -13,6 +13,10 @@ import Foundation
 /// Converting Berlin Time to Digital Time
 public enum BerlinClockRules {
     /// So what we want first is a way to get a textual representation of a Berlin Clock time based on a digital time. This is so we can use this converter everywhere, all we have to do is hook up a frontend. We're going to be going over the clock row by row to make things clearer and ensure we get everything right first time.
+    /// 00:00:00    YOOOOOOOOOOOOOOOOOOOOOOO
+    /// 23:59:59    ORRRRRRROYYRYYRYYRYYYYYY
+    /// 16:50:06    YRRROROOOYYRYYRYYRYOOOOO
+    /// 11:37:01    ORROOROOOYYRYYRYOOOOYYOO
     static public func berlinClock(from date: Date) -> BerlinClock {
         BerlinClock(seconds: "", singleMinutes: "", fiveMinutes: "", singleHours: "", fiveHours: "")
     }
--- BerlinClock/CoreTests/BerlinClockRulesTests.swift
+++ BerlinClock/CoreTests/BerlinClockRulesTests.swift
@@ -12,11 +12,23 @@ import Foundation
 
 struct CoreTests {
 
-    @Test("Convert date to BerlinClock fails")
-    func firstTest() {
-        let date = Date()
+    @Test("Convert date to BerlinClock succeds with 00:00:00")
+    func zeroTime() {
+        let date = DateFactory.create(hour: 0, minute: 0, second: 0)
         let berlinClock = BerlinClockRules.berlinClock(from: date)
         #expect(berlinClock.seconds == "")
     }
 
+}
+
+enum DateFactory {
+    static func create(hour: Int, minute: Int, second: Int) -> Date {
+        var dateComponents = DateComponents()
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+        dateComponents.second = second
+        
+        guard let date = Calendar.current.date(from: dateComponents) else { fatalError("couldn't create date")}
+        return date
+    }
 }
