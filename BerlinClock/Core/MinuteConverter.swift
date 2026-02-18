efce157688874a6bbbf6025b09d1ba96c70307a9
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-18 18:57:40 +0100

    Add acceptance criteria and prepare singleMinute first dummy implemenation

--- BerlinClock/Core/MinuteConverter.swift
+++ BerlinClock/Core/MinuteConverter.swift
@@ -8,6 +8,20 @@
 
 import Foundation
 
+/// Acceptance Criteria
+/// The upper row represents 5 minute blocks, and is made up of 11 lamps- every third lamp is red, the rest are yellow.
+/// The bottom row represents 1 minute blocks, and is made up of 4 yellow lamps.
 enum MinuteConverter {
-    
+    /// Acceptance Criteria
+    /// Given I have started the converter
+    /// When I enter $time
+    /// Then $row is returned for the single minutes row
+    /// 00:00:00    OOOO
+    /// 23:59:59    YYYY
+    /// 12:32:00    YYOO
+    /// 12:34:00    YYYY
+    /// 12:35:00    OOOO
+    static func singleMinute(from minute: Int) -> String {
+        ""
+    }
 }
