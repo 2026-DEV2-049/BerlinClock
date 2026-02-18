a394eac2e9f1f0392f2c27ad100b852cfbae21ac
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-18 17:39:06 +0100

    Start testing odd seconds SecondsConverter by making the test fail

--- BerlinClock/CoreTests/SecondsConverterTests.swift
+++ BerlinClock/CoreTests/SecondsConverterTests.swift
@@ -20,4 +20,11 @@ struct SecondsConverterTests {
         }
     }
 
+    @Test("Convert seconds to string succeeds for odd seocnds")
+    func conversionForEvenSeconds() {
+        for i in stride(from: 0, to: 59, by: 2) {
+            let result = SecondsConverter.convert(i)
+            #expect(result == "O")
+        }
+    }
 }
