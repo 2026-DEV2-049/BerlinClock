e37c73f5527a89dd93057ac9ccd49b84fe9d72ec
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-18 17:37:15 +0100

    Generalise the SecondsConverter test so that it succeeds for all even seconds

--- BerlinClock/CoreTests/SecondsConverterTests.swift
+++ BerlinClock/CoreTests/SecondsConverterTests.swift
@@ -14,8 +14,10 @@ struct SecondsConverterTests {
 
     @Test("Convert seconds to string succeeds for even seocnds")
     func conversionForEvenSeconds() {
-        let result = SecondsConverter.convert(0)
-        #expect(result == "Y")
+        for i in stride(from: 0, to: 59, by: 2) {
+            let result = SecondsConverter.convert(i)
+            #expect(result == "Y")
+        }
     }
 
 }
