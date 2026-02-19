d0c1d0ae07fb2e3dd1eb50ffed60cf645f23d668
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-19 11:59:59 +0100

    Group tests together and prepare a new Suite for BerlinClock to DigitalClock tests

--- BerlinClock/CoreTests/BerlinClockConverterTests.swift
+++ BerlinClock/CoreTests/BerlinClockConverterTests.swift
@@ -11,31 +11,38 @@ import Testing
 
 struct BerlinClockConverterTests {
 
-    @Test("Convert date to BerlinClock succeds with 00:00:00")
-    func zeroTime() {
-        let digitalClock = DigitalClock(hours: 0, minutes: 0, seconds: 0)
-        let berlinClock = BerlinClockConverter.berlinClock(from: digitalClock)
-        #expect(berlinClock.fullString == "YOOOOOOOOOOOOOOOOOOOOOOO")
-    }
+    @Suite("DigitalClock -> BerlinClock")
+    struct DigitalClockToBerlinClockTests {
+        @Test("Conversion succeds with 00:00:00")
+        func zeroTime() {
+            let digitalClock = DigitalClock(hours: 0, minutes: 0, seconds: 0)
+            let berlinClock = BerlinClockConverter.berlinClock(from: digitalClock)
+            #expect(berlinClock.fullString == "YOOOOOOOOOOOOOOOOOOOOOOO")
+        }
 
-    @Test("Convert date to BerlinClock succeds with 23:59:59")
-    func oneSecondBeforeMidnight() {
-        let digitalClock = DigitalClock(hours: 23, minutes: 59, seconds: 59)
-        let berlinClock = BerlinClockConverter.berlinClock(from: digitalClock)
-        #expect(berlinClock.fullString == "ORRRRRRROYYRYYRYYRYYYYYY")
+        @Test("Conversion succeeds with 23:59:59")
+        func oneSecondBeforeMidnight() {
+            let digitalClock = DigitalClock(hours: 23, minutes: 59, seconds: 59)
+            let berlinClock = BerlinClockConverter.berlinClock(from: digitalClock)
+            #expect(berlinClock.fullString == "ORRRRRRROYYRYYRYYRYYYYYY")
+        }
+        
+        @Test("Conversion succeeds with 16:50:06")
+        func fourPMFiftyMinutesAndSixSecond() {
+            let digitalClock = DigitalClock(hours: 16, minutes: 50, seconds: 06)
+            let berlinClock = BerlinClockConverter.berlinClock(from: digitalClock)
+            #expect(berlinClock.fullString == "YRRROROOOYYRYYRYYRYOOOOO")
+        }
+        
+        @Test("Conversion succeeds with 11:37:01")
+        func elevenHoursThirtySevenMinutesAndOneSecond() {
+            let digitalClock = DigitalClock(hours: 11, minutes: 37, seconds: 01)
+            let berlinClock = BerlinClockConverter.berlinClock(from: digitalClock)
+            #expect(berlinClock.fullString == "ORROOROOOYYRYYRYOOOOYYOO")
+        }
     }
     
-    @Test("Convert date to BerlinClock succeds with 16:50:06")
-    func fourPMFiftyMinutesAndSixSecond() {
-        let digitalClock = DigitalClock(hours: 16, minutes: 50, seconds: 06)
-        let berlinClock = BerlinClockConverter.berlinClock(from: digitalClock)
-        #expect(berlinClock.fullString == "YRRROROOOYYRYYRYYRYOOOOO")
-    }
-    
-    @Test("Convert date to BerlinClock succeds with 11:37:01")
-    func elevenHoursThirtySevenMinutesAndOneSecond() {
-        let digitalClock = DigitalClock(hours: 11, minutes: 37, seconds: 01)
-        let berlinClock = BerlinClockConverter.berlinClock(from: digitalClock)
-        #expect(berlinClock.fullString == "ORROOROOOYYRYYRYOOOOYYOO")
+    @Suite("BerlinClock -> DigitalClock")
+    struct BerlinClockToDigitalClockTests {
     }
 }
