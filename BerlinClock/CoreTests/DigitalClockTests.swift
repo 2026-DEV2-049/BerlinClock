bc616b5cfab09db9607dae8e720a020709a36e77
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-19 15:03:11 +0100

    Add failing tests for negative numbers

--- BerlinClock/CoreTests/DigitalClockTests.swift
+++ BerlinClock/CoreTests/DigitalClockTests.swift
@@ -14,9 +14,16 @@ struct DigitalClockTests {
     @Suite("Seconds init")
     struct SecondsTests {
         @Test("Throws OutOfBondError with seconds > 59")
-        func outOfBound() throws {
+        func outOfBound_Max() throws {
             #expect(throws: DigitalClock.Error.seconds(.outOfBond)) {
                 _ = try DigitalClock(hours: 0, minutes: 0, seconds: 60)
+            }
+        }
+        
+        @Test("Throws OutOfBondError with seconds < 0")
+        func outOfBound_negative() throws {
+            #expect(throws: DigitalClock.Error.seconds(.outOfBond)) {
+                _ = try DigitalClock(hours: 0, minutes: 0, seconds: -1)
             }
         }
     }
@@ -24,9 +31,16 @@ struct DigitalClockTests {
     @Suite("Minutes init")
     struct MinutesTests {
         @Test("Throws OutOfBondError with minutes > 59")
-        func outOfBound() throws {
+        func outOfBound_Max() throws {
             #expect(throws: DigitalClock.Error.minutes(.outOfBond)) {
                 _ = try DigitalClock(hours: 0, minutes: 60, seconds: 0)
+            }
+        }
+        
+        @Test("Throws OutOfBondError with minutes < 0")
+        func outOfBound_negative() throws {
+            #expect(throws: DigitalClock.Error.seconds(.outOfBond)) {
+                _ = try DigitalClock(hours: 0, minutes: -1, seconds: 0)
             }
         }
     }
@@ -34,9 +48,16 @@ struct DigitalClockTests {
     @Suite("Hours init")
     struct HoursTests {
         @Test("Throws OutOfBondError with hours > 24")
-        func outOfBound() throws {
+        func outOfBound_Max() throws {
             #expect(throws: DigitalClock.Error.hours(.outOfBond)) {
                 _ = try DigitalClock(hours: 24, minutes: 0, seconds: 0)
+            }
+        }
+        
+        @Test("Throws OutOfBondError with hours < 0")
+        func outOfBound_negative() throws {
+            #expect(throws: DigitalClock.Error.seconds(.outOfBond)) {
+                _ = try DigitalClock(hours: -1, minutes: 0, seconds: 0)
             }
         }
     }
