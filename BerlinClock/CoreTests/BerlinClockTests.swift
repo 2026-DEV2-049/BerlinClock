ee7c21cc5c7fbc40855e760e535b2d15fc2f599a
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-19 12:56:15 +0100

    Regroup tests together and adjust naming convention

--- BerlinClock/CoreTests/BerlinClockTests.swift
+++ BerlinClock/CoreTests/BerlinClockTests.swift
@@ -13,8 +13,8 @@ struct BerlinClockTests {
 
     @Suite("Seconds init")
     struct SecondsTests {
-        @Test("Throws invalid count")
-        func InvalidSecondCount() {
+        @Test("Throws invalid length")
+        func InvalidSecondLength() {
             #expect(throws: BerlinClock.Error.seconds(.invalidLength)) {
                 let invalidSecondCount = "YY"
                 _ = try BerlinClock(seconds: invalidSecondCount, singleMinute: "OOOO", fiveMinute: "OOOOOOOOOOO", singleHour: "0000", fiveHour: "0000")
@@ -32,19 +32,22 @@ struct BerlinClockTests {
     
     @Suite("Minutes init")
     struct MinutesTests {
-        @Test("Throws invalid count")
-        func InvalidSingleMinuteCount() {
-            #expect(throws: BerlinClock.Error.singleMinute(.invalidLength)) {
-                let invalidMinuteCount = "YY"
-                _ = try BerlinClock(seconds: "Y", singleMinute: invalidMinuteCount, fiveMinute: "OOOOOOOOOOO", singleHour: "0000", fiveHour: "0000")
+        @Suite("Single Minutes init")
+        struct SingleMinutesTests {
+            @Test("Throws invalid length")
+            func InvalidSingleMinuteLength() {
+                #expect(throws: BerlinClock.Error.singleMinute(.invalidLength)) {
+                    let invalidMinuteLength = "YY"
+                    _ = try BerlinClock(seconds: "Y", singleMinute: invalidMinuteLength, fiveMinute: "OOOOOOOOOOO", singleHour: "0000", fiveHour: "0000")
+                }
             }
-        }
-        
-        @Test("Throws invalid character")
-        func InvalidSingleMinuteCharacter() {
-            #expect(throws: BerlinClock.Error.singleMinute(.invalidCharacters)) {
-                let invalidMinuteCharacter = "ZZZZ"
-                _ = try BerlinClock(seconds: "Y", singleMinute: invalidMinuteCharacter, fiveMinute: "OOOOOOOOOOO", singleHour: "0000", fiveHour: "0000")
+            
+            @Test("Throws invalid character")
+            func InvalidSingleMinuteCharacter() {
+                #expect(throws: BerlinClock.Error.singleMinute(.invalidCharacters)) {
+                    let invalidMinuteCharacter = "ZZZZ"
+                    _ = try BerlinClock(seconds: "Y", singleMinute: invalidMinuteCharacter, fiveMinute: "OOOOOOOOOOO", singleHour: "0000", fiveHour: "0000")
+                }
             }
         }
     }
