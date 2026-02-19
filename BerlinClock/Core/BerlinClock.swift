dda148d80c07dda1461966503f5e699b4b0983e5
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-19 15:33:30 +0100

    Extract function into a more readable String extension

--- BerlinClock/Core/BerlinClock.swift
+++ BerlinClock/Core/BerlinClock.swift
@@ -34,28 +34,30 @@ public struct BerlinClock {
     
     public init(seconds: String, singleMinute: String, fiveMinute: String, singleHour: String, fiveHour: String) throws {
         guard seconds.count == 1 else { throw Error.seconds(.invalidLength) }
-        guard Self.isComposedOnlyOfSpecificLetters(seconds, allowedLetters: "YO") else { throw Error.seconds(.invalidCharacters) }
+        guard seconds.isComposedOnlyOf("YO") else { throw Error.seconds(.invalidCharacters) }
         self.seconds = seconds
         
         guard singleMinute.count == 4 else { throw Error.singleMinute(.invalidLength) }
-        guard Self.isComposedOnlyOfSpecificLetters(singleMinute, allowedLetters: "OY") else { throw Error.singleMinute(.invalidCharacters) }
+        guard singleMinute.isComposedOnlyOf("OY") else { throw Error.singleMinute(.invalidCharacters) }
         self.singleMinute = singleMinute
         
         guard fiveMinute.count == 11 else { throw Error.fiveMinute(.invalidLength) }
-        guard Self.isComposedOnlyOfSpecificLetters(fiveMinute, allowedLetters: "OYR") else { throw Error.fiveMinute(.invalidCharacters) }
+        guard fiveMinute.isComposedOnlyOf("OYR") else { throw Error.fiveMinute(.invalidCharacters) }
         self.fiveMinute = fiveMinute
         
         guard singleHour.count == 4 else { throw Error.singleHour(.invalidLength) }
-        guard Self.isComposedOnlyOfSpecificLetters(singleHour, allowedLetters: "OR") else { throw Error.singleHour(.invalidCharacters) }
+        guard singleHour.isComposedOnlyOf("OR") else { throw Error.singleHour(.invalidCharacters) }
         self.singleHour = singleHour
         
         guard fiveHour.count == 4 else { throw Error.fiveHour(.invalidLength) }
-        guard Self.isComposedOnlyOfSpecificLetters(fiveHour, allowedLetters: "OR") else { throw Error.fiveHour(.invalidCharacters) }
+        guard fiveHour.isComposedOnlyOf("OR") else { throw Error.fiveHour(.invalidCharacters) }
         self.fiveHour = fiveHour
     }
-    
-    static func isComposedOnlyOfSpecificLetters(_ string: String, allowedLetters: String) -> Bool {
+}
+
+extension String {
+    func isComposedOnlyOf(_ allowedLetters: String) -> Bool {
         let allowedSet = Set(allowedLetters)
-        return string.allSatisfy { allowedSet.contains($0) }
+        return self.allSatisfy { allowedSet.contains($0) }
     }
 }
