6d1bc6c75e0530a3e72f240bee0f2189c5bfc664
2026-DEV2-049 <2026DEV2049@gmail.com>
2026-02-20 11:48:56 +0100

    Improve readability by moving ViewModel into its own class

---
+++ BerlinClock/BerlinClock/BerlinClockViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  BerlinClockViewModel.swift
+//  BerlinClock
+//
+//  Created by 2026-DEV2-049 on 20/02/2026.
+//
+
+import SwiftUI
+import Core
+
+extension BerlinClockView {
+    struct Model {
+        let secondViewModel = SecondsView.Model()
+        let hourViewModel = DoubleRectangleView.Model()
+        let minuteViewModel = DoubleRectangleView.Model()
+        
+        func update(date: Date) {
+            guard let berlinClock = BerlinClockFactory.create(from: date) else { return }
+            secondViewModel.color = SecondsAdapter.color(from: berlinClock.seconds)
+            
+            hourViewModel.topRectangleViewModels = HoursAdapter.fiveHourRectangleModels(from: berlinClock.fiveHour)
+            hourViewModel.bottomRectangleViewModels = HoursAdapter.singleHourRectangleModels(from: berlinClock.singleHour)
+
+            minuteViewModel.topRectangleViewModels = HoursAdapter.fiveHourRectangleModels(from: berlinClock.fiveHour)
+            minuteViewModel.bottomRectangleViewModels = HoursAdapter.singleHourRectangleModels(from: berlinClock.singleHour)
+        }
+    }
+    
+    enum BerlinClockFactory {
+        static func create(from date: Date) -> BerlinClock? {
+            let calendar = Calendar.current
+            let components = calendar.dateComponents([.second, .minute, .hour], from: date)
+            guard let seconds = components.second else { return nil }
+            guard let minutes = components.minute else { return nil }
+            guard let hours = components.hour else { return nil }
+            
+            guard let digitalClock = try? DigitalClock(hours: hours, minutes: minutes, seconds: seconds) else { return nil }
+            guard let berlinClock = try? BerlinClockConverter.berlinClock(from: digitalClock) else { return nil }
+            return berlinClock
+        }
+    }
+    
+    enum SecondsAdapter {
+        static func color(from berlinClockSeconds: String) -> Color {
+            berlinClockSeconds == "Y" ? .yellow : .clear
+        }
+    }
+    
+    enum HoursAdapter {
+        static func singleHourRectangleModels(from berlinClockSingleHour: String) -> [RectangleView.Model] {
+            berlinClockSingleHour.map { RectangleView.Model(color: $0 == "R" ? .red : .clear) }
+        }
+        
+        static func fiveHourRectangleModels(from berlinClockFiveHour: String) -> [RectangleView.Model] {
+            berlinClockFiveHour.map { RectangleView.Model(color: $0 == "R" ? .red : .clear) }
+        }
+    }
+}
